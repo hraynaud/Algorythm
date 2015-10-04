1815cf723452dfdd68a78140f57482ef8486f906
hraynaud <herby.raynaud@gmail.com>
2015-10-03 22:35:45 -0400

    Initial Commit

---
+++ Algorythm/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Algorythm
+//
+//  Created by Herby Airbo Raynaud on 10/3/15.
+//  Copyright © 2015 Herby Airbo Raynaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Algorythm/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Algorythm
+//
+//  Created by Herby Airbo Raynaud on 10/3/15.
+//  Copyright © 2015 Herby Airbo Raynaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
