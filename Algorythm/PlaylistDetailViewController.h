d09c1dadd7af368df29aad9fd742b1f71917abbd
hraynaud <herby.raynaud@gmail.com>
2015-10-12 22:02:49 -0400

    WIP

---
+++ Algorythm/PlaylistDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PlaylistDetailViewController.h
+//  Algorythm
+//
+//  Created by Herby Airbo Raynaud on 10/4/15.
+//  Copyright © 2015 Herby Airbo Raynaud. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaylistDetailViewController : UIViewController
+
+@end
---
+++ Algorythm/PlaylistDetailViewController.m
@@ -0,0 +1,37 @@
+//
+//  PlaylistDetailViewController.m
+//  Algorythm
+//
+//  Created by Herby Airbo Raynaud on 10/4/15.
+//  Copyright © 2015 Herby Airbo Raynaud. All rights reserved.
+//
+
+#import "PlaylistDetailViewController.h"
+
+@interface PlaylistDetailViewController ()
+
+@end
+
+@implementation PlaylistDetailViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
